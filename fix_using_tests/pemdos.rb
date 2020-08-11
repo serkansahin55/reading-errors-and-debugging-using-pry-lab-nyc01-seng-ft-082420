a79eb3caca25bcec5ee103a67ab9c932efb524d4
Serkan Sahin <ssahin55@outlook.com>
2020-08-11 23:59:56 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,22 @@
 # don't forget to add: require 'pry'
 
+ 
+
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+  add_to_front  = "s" * 10
+  new_string = add_to_front + string
+    new_string
   else
     string
   end
 end
+
+
+
+# hash = {key: {key2: "value1"}, key3: {key4: "value2"}} 
+# hash.each do |key, value|
+  
+# end
